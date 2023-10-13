27cd7ac073c85ceb7c29dd9ea0ddacb0271ef3ed
Frank Fesevur <fesevur@gmail.com>
2023-10-14 00:11:27 +0200

    Add basic parsing of the timestamp

--- RebootAt.cpp
+++ RebootAt.cpp
@@ -1,7 +1,7 @@
 /**
- * 
+ *
  * Reboot the local machine at a given time
- * 
+ *
  */
 
 #ifndef UNICODE
@@ -9,10 +9,11 @@
 #endif
 
 #include <windows.h>
-#include <tchar.h>
-#include <stdio.h>
 #include <string>
 #include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <iomanip>
 
 /**
  * Get the human readable error message belonging to the given Windows error code value
@@ -107,11 +108,98 @@ static bool Reboot(DWORD secs, std::wstring msg, bool force)
 	return ret == ERROR_SUCCESS;
 }
 
+/**
+ * Convert a given timestamp to the number of seconds and the corresponding message
+ *
+ * @param timestamp The timestamp to parse
+ * @param secs The number of seconds from now until the given timestamp occurs
+ * @param timemsg The human readable time
+ *
+ * @return Returns `true` if the timestamp was successfully parsed
+ */
+static bool TimespanToSeconds(std::string timestamp, long& secs, std::wstring& timemsg)
+{
+	std::transform(timestamp.begin(), timestamp.end(), timestamp.begin(), ::toupper);
+	if (timestamp == "NOW")
+	{
+		secs = 0;
+		timemsg = L"NOW";
+		return true;
+	}
+
+	// Timestamp starting with a `+` means minutes
+	if (timestamp.at(0) == '+')
+	{
+		int minutes = std::stoi(timestamp.substr(1));
+		secs = minutes * 60;
+		timemsg = L"in " + std::to_wstring(minutes) + L" minutes";
+		return true;
+	}
+
+	// Timestamp containing a `:` must be a real timestamp
+	size_t colon = timestamp.find(":");
+	if (colon != std::string::npos)
+	{
+		int hour = std::stoi(timestamp.substr(0, colon));
+		int minute = std::stoi(timestamp.substr(colon + 1));
+
+		// Do some basic checking of the timestamp
+		if (hour < 0 || hour > 23 || minute < 0 || minute > 59)
+			return false;
+
+		// Calculate the number of seconds until we need to reboot
+		time_t now = time(NULL);	// number of seconds
+		tm loc;
+		localtime_s(&loc, &now);	// converted to a tm struct
+
+		// If the given hour is smaller then the current hour, it is the next day (now = 17:30, timestamp = 02:10)
+		// If the hours are the same and the given minutes is smaller, it the next day as well (now = 17:30, timestamp = 17:00)
+		if (hour < loc.tm_hour || (loc.tm_hour == hour && minute < loc.tm_min))
+		{
+			time_t nextday = time(NULL) + (24 * 60 * 60);
+			localtime_s(&loc, &nextday);
+		}
+
+		// Substitude the given hour and minute
+		loc.tm_hour = hour;
+		loc.tm_min = minute;
+		loc.tm_sec = 0;	// Always reboot the whole minute
+
+		// Convert the new time to number of seconds
+		time_t then = mktime(&loc);
+		secs = then - now;
+
+		// Construct the message
+		std::wostringstream stream;
+		stream << L"at " << std::setfill(L'0') << std::setw(2) << hour << L":" << std::setfill(L'0') << std::setw(2) << minute;
+		timemsg = stream.str();
+
+		return true;
+	}
+
+	return false;
+}
+
 int main(int argc, char * argv[])
 {
-	std::wstring msg = L"Rebooting in an hour";
+	// Is there a timestamp at the command line?
+	if (argc != 2)
+	{
+		std::cerr << "No timestamp given." << std::endl;
+		return 1;
+	}
 
-	if (Reboot(3600, msg, true))
+	// Convert the timestamp to the number of seconds
+	long secs;
+	std::wstring timemsg;
+	if (!TimespanToSeconds(argv[1], secs, timemsg))
+	{
+		std::cerr << "Invalid timestamp: " << argv[1] << std::endl;
+		return 1;
+	}
+
+	// Try to perform the reboot
+	if (Reboot(secs, L"Rebooting " + timemsg, true))
 		return 0;
 	return 1;
 }
