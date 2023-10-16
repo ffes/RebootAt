4004d94d07ef42da745baad2c9f6249b390bf599
Frank Fesevur <fesevur@gmail.com>
2023-10-16 21:37:13 +0200

    Add version from git
    Currently only visible in the VERSIONINFO in the executable
    Fixes #2

---
+++ version-git.bat
@@ -0,0 +1,41 @@
+@echo off
+
+REM Is Git for Windows x64 installed?
+if exist "%ProgramW6432%\Git\bin\bash.exe" (
+
+	echo Using bash from Git for Windows x64
+	setlocal
+	PATH=%ProgramW6432%\Git\mingw64\bin;%ProgramW6432%\Git\bin
+	bash version-git.sh
+	exit /b 0
+)
+
+REM Is Cygwin installed?
+if exist C:\Cygwin\bin\bash.exe (
+
+	echo Using bash from Cygwin
+	setlocal
+	PATH=C:\Cygwin\bin
+	bash version-git.sh
+	exit /b 0
+)
+
+REM Is Cygwin64 installed?
+if exist C:\Cygwin64\bin\bash.exe (
+
+	echo Using bash from Cygwin64
+	setlocal
+	PATH=C:\Cygwin64\bin
+	bash version-git.sh
+	exit /b 0
+)
+
+REM No Cygwin or Git for Windows installed
+REM If there is a file assume if is properly generated
+if exist version-git.h (
+	echo No bash found, but version-git.h exists
+	exit /b 0
+)
+
+echo No bash found
+exit /b 1
